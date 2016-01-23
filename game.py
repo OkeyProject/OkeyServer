14c6bede64958048f2ccccba87da8ccf8daacfd8
jubeatWwW <l6104400@gmail.com>
2016-01-23 22:35:20 +0800

    prettier code

--- game.py
+++ game.py
@@ -9,7 +9,37 @@ def getHand(s,hand):
     msg = {"status": 1,"cards":[{"color":i[0],"number":i[1]} for i in hand]}
     s.sendall(json.dumps(msg))
 
+def ThrowCard():
+    return true
 
+def TakeCard(data, turn ,cardStack, discard):
+    if not "from" in data:
+        return false, "Where to take card?"
+    if data['from'] != "deck" and data['from'] != "discard":
+        return false, "Wrong place"
+    if data['from'] == "deck":
+        if len(cardStack) == 0:
+            return false, "Deck is already empty"
+        newcard = cardStack.pop()
+        return true, json.dumps({"status": 1,"card":{"color":newcard[0],"number":newcard[1]}})
+    elif data['from'] == "discard":
+        if turn == 0:
+            LastPlayer = 3
+        else:
+            LastPlayer = turn - 1
+        if len(discard[LastPlayer]) == 0:
+            return false,"No cards there"
+        newcard = discard[LastPlayer].pop()
+        return true, json.dumps({"status": 1,"card":{"color":newcard[0],"number":newcard[1]}})
+
+def RecvDataChk(data,turn):
+    if not "player" in data or data['player'] < 1 or data['player'] > 4:
+        return false,"Player number fault"
+    if data['player'] != turn + 1:
+        return false, "Not this player's round"
+    if not "action" in data:
+        return false, "No action specified"
+    return true, ""
 
 
 def Game(s,logType):
@@ -19,6 +49,7 @@ def Game(s,logType):
     shuffle(cardStack)
 
     player = []
+    discard = [ [],[],[],[] ]
     for i in range(0,4):
         player.append([cardStack.pop() for j in range(0,14)])
         for j in range(0,10):
@@ -26,20 +57,28 @@ def Game(s,logType):
 
     turn = 0
     while True:
+        gameState = 0
         while True:
             data = json.dumps(s.recv(2048))
-            if "player" in data and data['player'] >=1 and data['player'] <=4:
-                if data['player'] == turn+1:
-                    if "action" in data:
-                        if data['action'] == "hand":
-                            getHand(s,player[turn])
-                    else:
-                        s.sendall(Err("No action specified"))
-                else:
-                    s.sendall(Err("Not this player's round"))
-            else:
-                s.sendall(Err("Player def error"))
 
+            errState,errMsg = RecvDataChk(data,turn)
+
+            if not errState:
+                s.sendall(Err(errMsg))
+
+            if data['action'] == "hand":
+                getHand(s,player[turn])
+            elif data['action'] == "take":
+                if gameState != 0:
+                    s.sendall(Err("You've take the card already"))
+                    continue
+                takeState, takeMsg = TakeCard(data, turn, cardStack, discard)
+                if takeState:
+                    s.sendall(takeMsg)
+                else:
+                    s.sendall(Err(takeMsg))
+            elif data['action'] == "throw":
+                ThrowCard()
 
 
         if turn == 3:
