360767640bafc085bfc69330dcb8f4f122bcec5c
jubeatWwW <l6104400@gmail.com>
2016-01-23 13:56:27 +0800

    Server with api declaration

--- restartServer.sh
+++ restartServer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ps auxww | grep cwlin | grep python | awk '{print $2}' | xargs kill -9
-python2.7 /u/cs/102/0216053/OkeyServer/main.py &
+python2.7 /u/cs/102/0216053/OkeyServer/server.py &
---
+++ server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+
+import socket
+import json
+import thread
+
+HOST = "140.113.235.151"
+PORT = 7975
+
+address = (HOST,PORT)
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind(address)
+s.listen(10)
+
+def Err(msg):
+    retmsg = {"status": 0, "message": msg}
+    return json.dumps(retmsg)
+
+def recvDataDecoder(data):
+    return json.loads(data)
+
+def main(s, addr):
+    data = recvDataDecoder(s.recv(2048))
+    if data['command'] == "game":
+        if "setting" in data and "log" in data['setting']:
+            print("Game Start")
+        else:
+            s.sendall(Err("Setting Error"))
+
+
+while True:
+    connection, address = s.accept()
+    thread.start_new_thread(main,(connection,address))
